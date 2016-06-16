5651ce09ed64ead3cfa26821d310a52f0db0742b
Md. Al-Amin opu <alamin.opu10@gmail.com>
2016-06-16 07:51:58 +0600

    Updated css style

--- public/single-notice.php
+++ public/single-notice.php
@@ -10,7 +10,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main wp-noticeboard" role="main">
 
 		<?php
 		// Start the loop.
@@ -53,10 +53,6 @@ get_header(); ?>
 				<?php
 				if ( in_array( get_post_type(), array( 'wp_noticeboard') ) ) {
 					$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-
-					if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-						$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-					}
 
 					$time_string = sprintf( $time_string,
 						esc_attr( get_the_date( 'c' ) ),
